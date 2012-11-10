7335baad87c93fa40e8e0b6a6d4d9f3e155ba9ae
Harper Jiang <harperjiang@msn.com>
2012-11-09 18:05:43 -0800

    New User Interface

---
+++ IDCalculator/IDCalculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  IDCalculator
+//
+//  Created by Harper Jiang on 11/9/12.
+//  Copyright (c) 2012 Harper Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
---
+++ IDCalculator/IDCalculator/GraphViewController.m
@@ -0,0 +1,38 @@
+//
+//  GraphViewController.m
+//  IDCalculator
+//
+//  Created by Harper Jiang on 11/9/12.
+//  Copyright (c) 2012 Harper Jiang. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
