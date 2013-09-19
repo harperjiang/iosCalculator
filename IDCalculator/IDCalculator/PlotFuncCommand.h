cd65becc560a1b84e5a01b68cbef2de75bf33c3f
Harper Jiang <harperjiang@msn.com>
2013-09-19 15:36:04 -0400

    Update Parser

---
+++ IDCalculator/IDCalculator/PlotFuncCommand.h
@@ -0,0 +1,13 @@
+//
+//  PlotCommand.h
+//  IDCalculator
+//
+//  Created by Harper Jiang on 9/18/13.
+//  Copyright (c) 2013 Harper Jiang. All rights reserved.
+//
+
+#import "Command.h"
+
+@interface PlotCommand : Command
+
+@end
---
+++ IDCalculator/IDCalculatorTests/NumberTest.h
@@ -0,0 +1,13 @@
+//
+//  NumberTest.h
+//  IDCalculator
+//
+//  Created by Harper Jiang on 9/18/13.
+//  Copyright (c) 2013 Harper Jiang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NumberTest : SenTestCase
+
+@end
